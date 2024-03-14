53838cbf074ace599836a144ee4e98ed0f757d08
seiren0729 ludium <plmikn31@gmail.com>
2024-03-14 11:39:29 +0000

    add aleo zkml first step

---
+++ run.sh
@@ -0,0 +1,9 @@
+APPNAME="classify_seiren"
+ENV_FILE=".env"
+PRIVATE_KEY=$(grep "PRIVATE_KEY" $ENV_FILE | cut -d '=' -f2)
+
+snarkos developer execute "${APPNAME}.aleo"  main '{e0:0u32, e1:0u32}' \
+  --private-key "${PRIVATE_KEY}" \
+  --query "https://api.explorer.aleo.org/v1" \
+  --priority-fee 3000000 \
+  --broadcast "https://api.explorer.aleo.org/v1/testnet3/transaction/broadcast" \
